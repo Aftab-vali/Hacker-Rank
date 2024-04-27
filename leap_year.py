dfaf2ca259e6b765ac182a72ab0ff4a8d2f2e303
Mohammed Aftab Vali <167098157+Aftab-vali@users.noreply.github.com>
2024-04-27 20:12:18 +0530

    Add files via upload
    the hacker rank solution are given and the solution for all the problems and code is written in the various format

---
+++ leap_year.py
@@ -0,0 +1,18 @@
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+    
+    # Write your logic here
+    
+    return leap
+
+year = int(input())
+print(is_leap(year))
